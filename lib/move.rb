4ac1c49417be4b1c6e7ff31a2b616c2cd0bd93e9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-22 20:25:48 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  puts "#{user_input}".to_i
-  return 
+  converted_input = "#{user_input}".to_i
+  return converted_input - 1
 end
   
   
