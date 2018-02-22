8989e3a4347075a892e5e2dac81ad910cc81ad76
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-22 20:14:54 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def index_to_value(user_input)
+def input_to_index(user_input)
   puts #{user_input}.to_i 
 end
   
